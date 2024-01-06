0544cd99d7b7d4e64285523d36df3c31737996b4
ZOR023 <aatishtandel23@gmail.com>
2024-01-06 21:17:55 +0530

    adding the latest fix

--- crawler.py
+++ crawler.py
@@ -1,6 +1,38 @@
 import requests 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
+import csv
 
-response = requests.get("https://scrapeme.live/shop/")
+countries = []
 
+urls = ["https://www.scrapethissite.com/pages/simple/"]
 
+while len(urls) != 0:
+  current_url = urls.pop()
+  
+  response = requests.get(current_url)
+  soup = BeautifulSoup(response.content, "html.parser")
+
+  link_elements = soup.select("a[href]")
+  
+  for link_element in link_elements:
+    url = link_element['href']
+    if "https://www.scrapethissite.com/pages/simple/" in url:
+      urls.append(url)
+  
+  country = {}
+  country["url"] = current_url
+  country["country_name"] = soup.select_one(".country-name").text
+  country["Capital"] = soup.select_one(".country-capital").text
+  country["Population"] = soup.select_one(".country-population").text
+  country["Area"] = soup.select_one(".country-area").text
+
+  countries.append(country)
+  
+with open('countries.csv', 'w') as csv_file:
+  writer = csv.DictWriter(csv_file, fieldnames=countries[0].keys())
+  
+  writer.writeheader
+  for country in countries:
+    writer.writerow(country)
+  
+  print(countries)
